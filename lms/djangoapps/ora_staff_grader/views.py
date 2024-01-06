f9a34c4c7f769edfc2551ee9b4312f054fffaedb
Vu Nguyen <vu.nguyen@dmttek.com>
2024-01-06 15:01:42 +0700

    test restrict staff manage grade permissions

--- lms/djangoapps/ora_staff_grader/views.py
+++ lms/djangoapps/ora_staff_grader/views.py
@@ -60,6 +60,11 @@ from openedx.core.djangoapps.content.course_overviews.api import (
 from openedx.core.djangoapps.waffle_utils import CourseWaffleFlag
 from openedx.core.lib.api.authentication import BearerAuthenticationAllowInactiveUser
 
+from common.djangoapps.student.roles import (
+    CourseStaffRole,
+)
+
+
 log = logging.getLogger(__name__)
 
 
@@ -360,6 +365,14 @@ class UpdateGradeView(StaffGraderBaseView):
     def post(self, request, ora_location, submission_uuid, *args, **kwargs):
         """Update a grade"""
         try:
+            # check manage grade permission.
+            ora_usage_key = UsageKey.from_string(ora_location)
+            course_id = str(ora_usage_key.course_key)
+            can_not_manage_grade = CourseStaffRole(course_id).can_manage_grade(request.user)
+            if can_not_manage_grade:
+                log.error(f"can_not_manage_grade: user: {request.user.username}, course {course_id}, submission {submission_uuid}")
+                return UnknownErrorResponse()
+
             # Reassert that we have ownership of the submission lock
             lock_info = check_submission_lock(request, ora_location, submission_uuid)
             if not lock_info.get("lock_status") == "in-progress":
