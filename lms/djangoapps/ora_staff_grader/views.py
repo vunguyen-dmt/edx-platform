106a3dc974d81d2f9c51250e251a03ae0485e20b
Vu Nguyen <vu.nguyen@dmttek.com>
2024-12-17 19:52:34 +0700

    prevent course staffs with beta testers role from managing grades.

--- lms/djangoapps/ora_staff_grader/views.py
+++ lms/djangoapps/ora_staff_grader/views.py
@@ -64,6 +64,9 @@ from openedx.core.djangoapps.content.course_overviews.api import (
 )
 from openedx.core.djangoapps.waffle_utils import CourseWaffleFlag
 from openedx.core.lib.api.authentication import BearerAuthenticationAllowInactiveUser
+from common.djangoapps.student.roles import (
+    CourseRole,
+)
 
 log = logging.getLogger(__name__)
 
@@ -461,6 +464,12 @@ class UpdateGradeView(StaffGraderBaseView):
     def post(self, request, ora_location, submission_uuid, *args, **kwargs):
         """Update a grade"""
         try:
+            ora_usage_key = UsageKey.from_string(ora_location)
+            can_not_manage_grade = CourseRole(role='staff_can_not_manage_grade',course_key=ora_usage_key.course_key).can_not_manage_grade(request.user)
+            if can_not_manage_grade:
+                log.error(f"can_not_manage_grade case ora_staff_grader_grade: user: {request.user.username}, course {str(ora_usage_key.course_key)}, submission {submission_uuid}")
+                return UnknownErrorResponse()
+
             # Reassert that we have ownership of the submission lock
             lock_info = check_submission_lock(request, ora_location, submission_uuid)
             if not lock_info.get("lock_status") == "in-progress":
@@ -534,6 +543,12 @@ class SubmissionLockView(StaffGraderBaseView):
     def post(self, request, ora_location, submission_uuid, *args, **kwargs):
         """Claim a submission lock"""
         try:
+            ora_usage_key = UsageKey.from_string(ora_location)
+            can_not_manage_grade = CourseRole(role='staff_can_not_manage_grade',course_key=ora_usage_key.course_key).can_not_manage_grade(request.user)
+            if can_not_manage_grade:
+                log.error(f"can_not_manage_grade case ora_staff_grader_lock: user: {request.user.username}, course {str(ora_usage_key.course_key)}, submission {submission_uuid}")
+                return UnknownErrorResponse()
+
             # Validate ORA location
             UsageKey.from_string(ora_location)
             lock_info = claim_submission_lock(request, ora_location, submission_uuid)
